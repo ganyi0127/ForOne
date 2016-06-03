1939827be005fd74158b8cac08652c18322abaab
System Administrator <root@ganyi.local>
2016-06-04 00:32:04 +0800

    fix

--- nodejs_app/database.js
+++ nodejs_app/database.js
@@ -8,20 +8,14 @@ function connectDB(){
 		host : 'localhost',
 		port : '3306',
 		user : 'root',
-<<<<<<< HEAD
-		password : '123456'
-	});
-=======
     password : '123'
   });
->>>>>>> 2fa23c90ffdfdec55ba6ee9df74403bdcd410c9a
 
 	//2.执行SQL语句，使用FSTMB数据库
 	conn.query('USE foronedb');
 
 	return conn;
 }
-
 function closeDB(conn){
 	//3.关闭数据库连接
 	conn.end();
